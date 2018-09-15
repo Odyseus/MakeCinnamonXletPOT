573c886d79b4ffa2597e593d1f35787290bff805
Odyseus <venusyadonis@gmail.com>
2018-09-15 01:25:45 -0300

    Bump version.

--- AppData/MakeCinnamonXletPOTApp/__init__.py
+++ AppData/MakeCinnamonXletPOTApp/__init__.py
@@ -4,4 +4,4 @@
 __status__ = "(Stable)"
 __appname__ = "Make Cinnamon Xlet POT"
 __appdescription__ = "A CLI utility to generate POT files for Cinnamon xlets."
-__version__ = "2018-06-12 02:20:15.236"
+__version__ = "2018-09-15 01:19:13.335"
