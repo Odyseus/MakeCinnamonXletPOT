6e7221398b2aaab0d65a9fdc9875b4954e4fe1d2
Odyseus <venusyadonis@gmail.com>
2018-09-16 04:39:15 -0300

    Added README and updated __init__.py module.

--- AppData/MakeCinnamonXletPOTApp/__init__.py
+++ AppData/MakeCinnamonXletPOTApp/__init__.py
@@ -5,3 +5,7 @@ __status__ = "(Stable)"
 __appname__ = "Make Cinnamon Xlet POT"
 __appdescription__ = "A CLI utility to generate POT files for Cinnamon xlets."
 __version__ = "2018-09-15 01:19:13.335"
+
+
+if __name__ == "__main__":
+    pass
---
+++ README.md
@@ -0,0 +1,4 @@
+
+## Make Cinnamon Xlet POT ([documentation](https://pythoncliapplications.gitlab.io/CLIApplicationsManager/includes/MakeCinnamonXletPOT/index.html))
+
+A CLI utility to generate POT files for Cinnamon xlets.
