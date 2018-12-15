e365722bd21523142fe68e18b4651adf7c28fe2c
Odyseus <venusyadonis@gmail.com>
2018-12-15 07:29:34 -0300

    __init__.py
    - Homogenization of the __appdescription__ variable.

--- AppData/MakeCinnamonXletPOTApp/__init__.py
+++ AppData/MakeCinnamonXletPOTApp/__init__.py
@@ -3,7 +3,7 @@
 
 __status__ = "Stable"
 __appname__ = "Make Cinnamon Xlet POT"
-__appdescription__ = "A CLI utility to generate POT files for Cinnamon xlets."
+__appdescription__ = "CLI utility to generate POT files for Cinnamon xlets."
 __version__ = "2018-09-15 01:19:13.335"
 
 
