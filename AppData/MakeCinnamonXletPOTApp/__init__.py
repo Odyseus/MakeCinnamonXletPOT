02221eff8cbba1aed9233f1016e0fa29d2c62873
Odyseus <venusyadonis@gmail.com>
2020-03-08 09:13:32 -0300

    Updated manual page and bump version

--- AppData/MakeCinnamonXletPOTApp/__init__.py
+++ AppData/MakeCinnamonXletPOTApp/__init__.py
@@ -10,4 +10,4 @@ __appname__ = "Make Cinnamon Xlet POT"
 
 __appdescription__ = "CLI utility to generate POT files for Cinnamon xlets."
 
-__version__ = "2020-02-19 12:00:13.466"
+__version__ = "2020-03-08 09:10:52.473"
