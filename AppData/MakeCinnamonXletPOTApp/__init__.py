26321244fbc724e179fdb8c56dc0026ae7a56f16
Odyseus <venusyadonis@gmail.com>
2020-04-11 21:40:36 -0300

    Corrected __init__.py

--- AppData/MakeCinnamonXletPOTApp/__init__.py
+++ AppData/MakeCinnamonXletPOTApp/__init__.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-
 # -*- coding: utf-8 -*-
-
-
-
 __status__ = "Stable"
-
 __appname__ = "Make Cinnamon Xlet POT"
-
 __appdescription__ = "CLI utility to generate POT files for Cinnamon xlets."
+__version__ = "2020-04-11 06:23:52.163"
 
-__version__ = "2020-03-20 21:47:57.129"
+if __name__ == "__main__":
+    pass
